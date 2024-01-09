4a0963ca73834baeebdbc872521973e48f0160d5
Szécsi Tamás Dávid <144216869+sz3szetam@users.noreply.github.com>
2024-01-09 19:42:23 +0100

    Kész projekt

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-function Fetcheles()
-{
-    fetch("https://jsonplaceholder.typicode.com/todos")
+let currentPage = 1;
+const itemsPerPage = 20;
+
+function Fetcheles(page) {
+    fetch(`https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=${itemsPerPage}`)
     .then(valasz => valasz.json())
     .then(valasz => Adatmegjelenites(valasz))
     .catch(hiba => console.log(hiba));
 } 
-function Adatmegjelenites(valasz)
-{
-    console.log(valasz)
-    let table = '<table>'
-    table += '<tr><td><Felhasználó</td><td>Sorszám</td><td>Leírás</td></tr>'
-    valasz.forEach(item =>
-        {
-            table += `<tr>
-            <td>${item.userId}</td>
-            <td>${item.title}</td>
-            <td>${item.id}</td>
-            </tr>`
-        });
-        table += '</table>'
-        document.getElementById('content').innerHTML = table;
+
+function Adatmegjelenites(valasz) {
+    console.log(valasz);
+    let table = '<table>';
+    table += '<tr><td>Felhasználó</td><td>Sorszám</td><td>Leírás</td></tr>';
+    valasz.forEach(item => {
+        table += `<tr>
+        <td>${item.userId}</td>
+        <td>${item.id}</td>
+        <td>${item.title}</td>
+        </tr>`;
+    });
+    table += '</table>';
+    document.getElementById('content').innerHTML = table;
 }
-window.onload = Fetcheles()
+
+document.getElementById("elozo").addEventListener("click",() =>
+{
+    if (currentPage > 1) {
+        currentPage--;
+        Fetcheles(currentPage);
+    }
+});
+document.getElementById("kovetkezo").addEventListener("click",() =>
+{
+    currentPage++;
+    Fetcheles(currentPage);
+})
+
+window.onload = Fetcheles(currentPage);
